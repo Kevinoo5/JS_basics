b411f8ea52c57b643c0d717d6460e2b62fc06336
Kevinoo5 <kevin.noormets@voco.ee>
2022-09-20 11:30:44 +0300

    1. commit

---
+++ app.js
@@ -0,0 +1,54 @@
+// Strings
+let name = "Kevin Noormets"
+console.log(name)
+console.log(typeof name)
+
+let goodMark = 5
+console.log(goodMark)
+console.log(typeof goodMark)
+
+let percent = 50.3
+console.log(percent)
+console.log(typeof percent)
+
+let isHere = true // false
+console.log(isHere)
+console.log(typeof isHere)
+
+// const
+
+const surname = "Noormets"
+// surname = "Eeee" - error
+
+// arrays
+
+const numbers = [1,2,3,4,5]
+console.log(numbers)
+numbers.push(6)
+console.log(typeof numbers)
+
+// objects
+const person = {
+    firstname: "Kevin",
+    surname: "Noormets"
+}
+console.log(person)
+console.log(person.firstname)
+console.log(person.surname)
+person.department = "IKT"
+console.log(typeof person)
+
+// Null
+const Airplane = null
+console.log(Airplane)
+console.log(typeof Airplane)
+
+// Symbol
+const symbol = Symbol()
+console.log(symbol)
+console.log(typeof symbol)
+
+// undefined
+let test
+console.log(test)
+console.log(typeof test)
