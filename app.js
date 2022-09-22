0d0761eb03b6fe3419481d4768676d47ff705d35
Kevinoo5 <kevin.noormets@voco.ee>
2022-09-22 13:44:18 +0300

    commit

--- app.js
+++ app.js
@@ -1,18 +1,53 @@
-const today = new Date()
+/*
+if(condition){
+    // if statements
+} else {
+    // else statements
+}
 
-let val
 
-val = today
-val = today.getMonth()
-val = today.getDate()
-val = today.getDay()
-val = today.getFullYear()
-val = today.getHours()
-val = today.getMinutes()
-val = today.getTime()
+// == equal --> ===
+// != not equal --> !==
+*/
 
-val = new Date("09/22/2021")
-val = new Date(23)
-val.setFullYear(2023)
+let personID;
 
-console.log(val)
+if(typeof personID !== "undefined"){
+    console.log("id not correct")
+} else {
+    console.log(`id is ${personID}`)
+}
+
+console.log(personID)
+
+const name = "Kadi"
+const age = 60
+
+if(age > 0 && age <= 12){
+    console.log(`${name} on laps`)
+} else if(age > 12 && age < 18){
+    console.log(`${name} on nooruk`)
+} else {
+    console.log(`${name} on täiskasvanu`)
+}
+
+
+let id = 5
+if(id === 100){
+    console.log("Ok")
+} else {
+    console.log("Not")
+}
+
+// ? - ternary
+console.log(id === 100 ? "Ok" : "Not")
+
+// switch
+const color = "red"
+
+switch (color) {
+    case "red":
+        console.log("Stop!")
+        break;
+
+}
