2ac31e8b9386d5b0177cff46cbe197af03a38cc5
Kevinoo5 <kevin.noormets@voco.ee>
2022-09-22 12:41:52 +0300

    First commit

--- app.js
+++ app.js
@@ -1,34 +1,7 @@
-const numbers1 = [43,56,33,23,44,36,5]
-const numbers2 = new Array(22,45,33,76,54)
-
-let val
-
-val = numbers1.length
-val = numbers2.length
-
-val = numbers1[3]
-val = numbers1.indexOf(36)
-
-val = Array.isArray(numbers2)
-
-numbers1.push(250)
-numbers1.unshift(120)
-numbers1.shift()
-
-val = numbers1.concat(numbers2)
-
-
-const fruits = ["banana", "apple", "orange"]
-console.log(fruits)
-fruits.sort()
-console.log(fruits)
-
-val = numbers1.sort(function (x,y) {
-    return x - y
-})
-console.log(val)
-
-val = numbers1.sort(function (x,y) {
-    return y - x
-})
-console.log(val)
+const person = {
+    firstname: "Kadi",
+    surname: "Tamm",
+    age: 40,
+    email:"kadi.tamm@gmail.com",
+    hobbies:["tennis", "korvpall"],
+}
