b68e453a2e076b6de18d489ea5ab1ed8bdd4423f
Kevinoo5 <kevin.noormets@voco.ee>
2022-09-22 13:10:18 +0300

    1. commit

--- app.js
+++ app.js
@@ -4,4 +4,31 @@ const person = {
     age: 40,
     email:"kadi.tamm@gmail.com",
     hobbies:["tennis", "korvpall"],
+    address:{
+        city:"Viru",
+        county:"Võrumaa"
+    },
+    getBirthYear: function (){
+        return 2022 - this.age
+    }
 }
+
+let val
+
+val = person
+val = person.firstname
+val = person["surname"]
+val = person["hobbies"][1]
+val = person.hobbies[0]
+val = person.address.city
+val = person.address["city"]
+val = person.getBirthYear()
+console.log(val)
+
+for (i = 0; i < person.hobbies.length; i++){
+    console.log(person.hobbies[i])
+}
+
+person.hobbies.forEach((hobbie, index) => {
+    console.log(hobbie + " on element indeksiga " + index)
+})
